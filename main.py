58d38c5c5e438a646a301f5d2395b44de9cd111f
Vegard Hansen Stenberg <vegard.koder@gmail.com>
2020-10-11 13:08:56 +0200

    set up the basic page with 1 article. The flask server is up and everything looks cool

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import get_data
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello"
+    return render_template("index.html", link=get_data.get_google_link(get_data.get_articles()[0])[2], name=get_data.get_google_link(get_data.get_articles()[0])[1], date=get_data.get_google_link(get_data.get_articles()[0])[0])
 
 if __name__ == '__main__':
     app.run()
