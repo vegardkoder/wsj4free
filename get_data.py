6857e68dbc89295c6875ebceea0aa81639816810
Vegard Hansen Stenberg <vegard.koder@gmail.com>
2020-10-11 12:48:20 +0200

    Did some changes, now you get similar webpages instead. Also started working on the webpage using flask

---
+++ get_data.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+from requests import get
+from urllib.request import Request, urlopen
+import webbrowser
+
+def get_articles():
+    url = 'https://www.wsj.com/'
+    response = urlopen(Request(url, headers={'User-Agent': 'Mozilla/5.0'}))
+    soup = BeautifulSoup(response.read(), features="lxml")
+
+    articles = soup.find_all("div", {"class": "WSJTheme--headline--7VCzo7Ay"})
+    headers = []
+
+    for article in articles:
+        headers.append(article.h3.a.text)
+
+    return headers
+
+def get_google_link(header):
+    h = header.replace(' ', '+')
+    url = "https://www.google.com/search?q=" + h
+    response = urlopen(Request(url, headers={'User-Agent': 'Mozilla/5.0'}))
+    soup = BeautifulSoup(response.read(), features="lxml")
+
+    result = soup.find_all("div", {"class": "kCrYT"})[2]
+
+    link = result.a['href'][7:].split("&")[0]
+    name = result.text.split("»")[0]
+    date = soup.find_all("div", {"class": "kCrYT"})[3].text.split("·")[0]
+
+    return [date, name, link]
--- main.py
+++ main.py
@@ -1,29 +1,11 @@
-from bs4 import BeautifulSoup
-from requests import get
-from urllib.request import Request, urlopen
+import get_data
+from flask import Flask
 
-def get_articles():
-    url = 'https://www.wsj.com/'
-    response = urlopen(Request(url, headers={'User-Agent': 'Mozilla/5.0'}))
-    soup = BeautifulSoup(response.read(), features="lxml")
+app = Flask(__name__)
 
-    articles = soup.find_all("div", {"class": "WSJTheme--headline--7VCzo7Ay"})
-    headers = []
+@app.route("/")
+def home():
+    return "Hello"
 
-    for article in articles:
-        headers.append(article.h3.a.text)
-
-    return headers
-
-def get_google_link(header):
-    h = header.replace(' ', '+')
-    print(h)
-    url = "https://www.google.com/search?q=" + h
-    response = urlopen(Request(url, headers={'User-Agent': 'Mozilla/5.0'}))
-    soup = BeautifulSoup(response.read(), features="lxml")
-
-    result = soup.find("div", {"class": "kCrYT"}).a['href'][7:].split("&")[0]
-
-    return result
-
-print(get_google_link(get_articles()[0]))
+if __name__ == '__main__':
+    app.run()
